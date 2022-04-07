3d3d3d2d686cded7d0fa1964a04c5b5a27490c32
Vishnu Prasad M <vpm735@gmail.com>
2022-04-07 22:32:09 +0530

    backend test postman

--- backend/src/server.js
+++ backend/src/server.js
@@ -20,41 +20,104 @@ import { MongoClient } from 'mongodb';
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/api/articles/:name', async (req, res) => {
+const withDB = async (operations,res) => {
     try {
-        const articleName = req.params.name;
+
         const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true })
         const db = client.db('my-blog');
-        const articleInfo = await db.collection('articles').findOne({ name: articleName })
-        res.status(200).json(articleInfo);
+        await operations(db)
         client.close()
     }
     catch (error) {
         res.status(500).json({ message: 'Error Not Found', error })
     }
+}
 
+
+// app.get('/api/articles/:name', async (req, res) => {
+//     try {
+//         const articleName = req.params.name;
+//         const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true })
+//         const db = client.db('my-blog');
+//         const articleInfo = await db.collection('articles').findOne({ name: articleName })
+//         res.status(200).json(articleInfo);
+//         client.close()
+//     }
+//     catch (error) {
+//         res.status(500).json({ message: 'Error Not Found', error })
+//     }
+// })
+//Making more Reusable code using withDB function
+app.get('/api/articles/:name', async (req, res) => {
+    withDB(async (db) => {
+        const articleName = req.params.name;
+        const articleInfo = await db.collection('articles').findOne({ name: articleName })
+        res.status(200).json(articleInfo);
+    },res)
 })
 
 app.post('/api/articles/:name/upvote', async (req, res) => {
-  try{
-    const articleName = req.params.name;
-    const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true })
-    const db = client.db('my-blog');
-    const articleInfo = await db.collection('articles').findOne({ name: articleName });
-    await db.collection('articles').updateOne({name: articleName},{$set: {
-        upvotes: articleInfo.upvotes + 1
-    }})
-    const updatedArticleinfo = await db.collection('articles').findOne({ name: articleName });
-    res.status(200).json(updatedArticleinfo)
-    client.close
-  }
-  catch (error) {
-    res.status(500).json({ message: 'Error Not Found', error })
-}
+    withDB(async (db) => {
+        const articleName = req.params.name;
+       
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({ name: articleName }, {
+            $set: {
+                upvotes: articleInfo.upvotes + 1
+            }
+        })
+        const updatedArticleinfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(updatedArticleinfo)
+   
     // articleInfo[articleName].upvotes += 1
     // res.status(200).send(`${articleName} now has ${articleInfo[articleName].upvotes}  upvotes `)
     // res.send(`"Hello" ${req.params.name}`)
+    },res);
 })
+
+app.post('/api/articles/:name/addcmt', async (req, res) => {
+    withDB(async (db) => {
+        const {username,text} = req.body;
+        const articleName = req.params.name;
+       
+        const articleInfo = await db.collection('articles').findOne({ name: articleName });
+        await db.collection('articles').updateOne({ name: articleName }, {
+            $set: {
+                comments : articleInfo.comments.concat({username , text})
+            }
+        })
+        const updatedArticleinfo = await db.collection('articles').findOne({ name: articleName });
+        res.status(200).json(updatedArticleinfo)
+   
+    // articleInfo[articleName].upvotes += 1
+    // res.status(200).send(`${articleName} now has ${articleInfo[articleName].upvotes}  upvotes `)
+    // res.send(`"Hello" ${req.params.name}`)
+    },res);
+})
+//Before WithDB
+// app.post('/api/articles/:name/upvote', async (req, res) => {
+//     try {
+//         const articleName = req.params.name;
+//         const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true })
+//         const db = client.db('my-blog');
+//         const articleInfo = await db.collection('articles').findOne({ name: articleName });
+//         await db.collection('articles').updateOne({ name: articleName }, {
+//             $set: {
+//                 upvotes: articleInfo.upvotes + 1
+//             }
+//         })
+//         const updatedArticleinfo = await db.collection('articles').findOne({ name: articleName });
+//         res.status(200).json(updatedArticleinfo)
+//         client.close
+//     }
+//     catch (error) {
+//         res.status(500).json({ message: 'Error Not Found', error })
+//     }
+//     // articleInfo[articleName].upvotes += 1
+//     // res.status(200).send(`${articleName} now has ${articleInfo[articleName].upvotes}  upvotes `)
+//     // res.send(`"Hello" ${req.params.name}`)
+// })
+//Before MongoDB
 // app.post('/api/articles/:name/addcmt',(req,res)=>{
 //     const {username,text} = req.body;
 //     const articleName = req.params.name;
